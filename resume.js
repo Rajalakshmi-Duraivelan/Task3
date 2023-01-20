aa461a80c15a61d4985c840a3c6bd34d4bde5eb8
Rajalakshmi-Duraivelan <rajydurai.02@gmail.com>
2023-01-20 19:16:13 +0530

    Task 3

---
+++ resume.js
@@ -0,0 +1,71 @@
+let myResume={
+    "basics": {
+      "name": "Rajalakshmi Duraivelan",
+      "email": "rajydurai.02@gmail.com",
+      "contactno": 9566262122,
+      "degree": "Bachelor of Engineering",
+      "address": {
+        "AddressLine1": "5/2, Subhiksha Flats, GB-Ground Floor",
+        "AddressLine2": "Trustpuram, 6th cross st, Kodambakkam",
+        "city": "Chennai",
+        "state": "Tamilnadu",
+        "zipCode": 600024,
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "network": "LinkedIn",
+          "userName": "Rajalakshmi Duraivelan",
+          "url": "https://www.linkedin.com/in/rajalakshmiduraivelan/"
+        },
+        {
+          "network": "GITHub",
+          "userName": "Rajalakshmi-Duraivelan",
+          "url": "https://github.com/Rajalakshmi-Duraivelan"
+        }
+      ]
+    },
+    "worksummary": [
+      {
+        "company": "Ascendion Pvt Ltd",
+        "designation": "Engineer",
+        "startdate": "2021-06-16",
+        "enddate": "2023-01-11",
+        "summary": "Analyzing the user requirements, developing the applications, Issue fixing, handling data and stored procedure in database.",
+      },
+    ],
+    "education": [
+      {
+        "institution": "Prince Dr.K. Vasudevan College of Engg & Tech",
+        "department": "Computer Science",
+        "yearofpassing": 2015,
+        "cgpa": 7.8
+      },
+      {
+        "institution": "Accord Infomatrix",
+        "department": "DotNet",
+        "yearofpassing": 2015
+      },
+      {
+        "institution": "Accord Infomatrix",
+        "department": "Software Testing",
+        "yearofpassing": 2015
+      }
+    ],
+    "skills": [
+      {
+        "name": "C#, MVC",
+        "database": "MSSQL",
+        "project": [
+          "CVS Healthcare",
+          "NYS Personal Income Tax"
+        ]
+      }
+    ],
+    "Languages": [
+      {
+        "Language": "Tamil, English",
+      }
+    ]
+  }
+  console.log(myResume);
---
+++ script.js
@@ -0,0 +1,42 @@
+
+var json = { 
+    "userdetails":[{
+      "id": 1,
+      "first_name": "John",
+      "last_name": "Doe",
+      "email": "john123@guvi.in"
+    },
+    {
+      "id": 2,
+      "first_name": "Lucy",
+      "last_name": "Ballmer",
+      "email": "lucy456@guvi.in"
+    },
+    {
+      "id": 3,
+      "first_name": "Anna",
+      "last_name": "Smith",
+      "email": "anna789@guvi.in"
+    }],
+    "userlist" : ["john123@guvi.in","lucy456@guvi.in","anna789@guvi.in"]
+};
+//For loop
+for(let i=0;i<json.length;i++){
+    var obj = json[i];
+    console.log(obj.id);
+    console.log(obj.first_name);
+    console.log(obj.last_name);
+    console.log(obj.email);
+}
+
+//For-In Loop
+for(var i in json.email){
+    console.log(i);
+}
+
+//For-Of Loop
+for(var x of json.userlist){
+console.log(x);
+}
+
+//For-Each Loop
